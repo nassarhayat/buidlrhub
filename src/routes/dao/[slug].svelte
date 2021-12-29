7726c6da5febc5f38f84385db9808960a5606dfb
Nassar Hayat <nassar@makerlabs.co.uk>
2021-12-29 13:53:33 +0000

    move to sveltekit

---
+++ src/routes/dao/[slug].svelte
@@ -0,0 +1,89 @@
+<script context="module">
+	import { page } from '$app/stores';
+	import funded from "../../data/funded.json"
+</script>
+
+
+<section>
+	<div class="partner">
+		<div>{$page.params.slug}</div>
+		<div>funding range</div>
+	</div>
+	<div class="cardsheader">
+		<h2>partner</h2>
+	</div>
+	<div class="container">
+		<div class="fundedheader">
+			<h2>funded</h2>
+		</div>
+		{#each funded as f}
+			<a href={`/${f.name}`}>
+			<div class="fundedcard">
+				<div class="avatarrow">
+				<img class="avatar" src={f.image_url} alt="logo" />
+				<div>
+					<h4>{f.name}</h4>
+					<div class="links">
+					<p>{f.amount}</p>
+					<a href={f.website_url} target="_blank">learn more</a>
+					</div>
+				</div>
+				</div>
+			</div>
+			</a>
+		{/each}
+	</div>
+</section>
+
+<style>
+	section {
+		margin-top: 36px;
+		display: grid;
+		grid-template-columns: 0.5fr 1fr 0.5fr;
+	}
+	.cardsheader {
+		background-color: white;
+		padding: 1rem;
+		border-bottom:1px solid rgba(0, 0, 0, 0.3);
+	}
+	.partner {
+		color: white;
+	}
+	.container {
+	  margin-left: 1rem;
+	}
+	.fundedheader {
+	  color: white;
+	  padding: 1rem;
+	}
+	.fundedcard {
+	  background-color: #161b22;
+	  border: 1px solid black;
+	  color: white;
+	  padding: 1rem;
+	}
+	.avatarrow {
+	  display: flex;
+	  flex-direction: row;
+	  grid-gap: 1rem;
+	}
+	.avatar {
+	  border-radius: 100%;
+	  width: 15vw;
+	  height: 15vw;
+	}
+	p, a {
+	  margin: 5px 0;
+	}
+	.links {
+	  display: flex;
+	  flex-direction: row;
+	  grid-gap: 10px;
+	}
+	@media (min-width: 640px) { 
+	  .avatar {
+		width: 3vw;
+		height: 3vw;
+	  }
+	}
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,9 +1,11 @@
 <script context="module" lang="ts">
+	import partners from "../data/partners.json"
+	import funded from "../data/funded.json"
 	export const prerender = true;
 </script>
 
 <script lang="ts">
-	import Counter from '$lib/Counter.svelte';
+	
 </script>
 
 <svelte:head>
@@ -11,49 +13,146 @@
 </svelte:head>
 
 <section>
-	<h1>
-		<div class="welcome">
-			<picture>
-				<source srcset="svelte-welcome.webp" type="image/webp" />
-				<img src="svelte-welcome.png" alt="Welcome" />
-			</picture>
+	<div>
+		<h1>Grants and investment from DAOs</h1>
+		<h3>Your single hub for applying for grants and incubator programmes</h3>
+	</div>
+	<div>
+		<div class="cardsheader">
+			<h2>partners</h2>
 		</div>
-
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/index.svelte</strong>
-	</h2>
-
-	<Counter />
+		{#each partners as partner}
+			<a href={`/dao/${partner.name}`}>
+				<div class="card">
+				<img class="avatar" src={partner.image_url} alt="logo" />
+				<h4 class="cardtitle">{partner.name}</h4>
+				<p>{partner.description}</p>
+				<div class="options">
+					{#if partner.twitter_url}
+					<a href={partner.twitter_url} target="_blank">
+						<img class="twitter" src="/logos/twitter.png" alt="twitter" />
+					</a>
+					{/if}
+					<a class="programmeButton" href={partner.programme_url} target="_blank">Programme info</a>
+				</div>
+				</div>
+			</a>
+		{/each}
+	</div>
+	<div class="container">
+		<div class="fundedheader">
+			<h2>funded</h2>
+		</div>
+		{#each funded as f}
+			<a href={`/${f.name}`}>
+			<div class="fundedcard">
+				<div class="avatarrow">
+				<img class="avatar" src={f.image_url} alt="logo" />
+				<div>
+					<h4>{f.name}</h4>
+					<div class="links">
+					<p>{f.amount}</p>
+					<a href={f.website_url} target="_blank">learn more</a>
+					</div>
+				</div>
+				</div>
+			</div>
+			</a>
+		{/each}
+	</div>
 </section>
 
 <style>
 	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 1;
+		margin-top: 36px;
+		display: grid;
+		grid-template-columns: 0.5fr 1fr 0.5fr;
 	}
-
-	h1 {
-		width: 100%;
+	h1, h3 {
+	  color: white;
 	}
-
-	.welcome {
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
+	h3 {
+	  margin-top: 20px;
 	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
+	.cardsheader {
+	  background-color: white;
+	  padding: 1rem;
+	  border-bottom:1px solid rgba(0, 0, 0, 0.3);
 	}
-</style>
+	.card {
+	  background: white;
+	  padding: 1rem;
+	  border-bottom:1px solid rgba(0, 0, 0, 0.3);
+	  align-self: flex-end;
+	}
+  
+	.avatar {
+	  border-radius: 100%;
+	  width: 15vw;
+	}
+  
+	.options {
+	  display: flex;
+	  flex-direction: row;
+	  grid-gap: 10px;
+	}
+  
+	.twitter {
+	  background-color: #00ACEE;
+	  max-width: 20px;
+	  padding: 5px;
+	  border-radius: 10px;
+	}
+  
+	.programmeButton {
+	  border: solid 2px black;
+	  border-radius: 10px;
+	  padding: 10px;
+	}
+	h4 {
+	  margin-top: 1vh;
+	}
+	.container {
+	  margin-left: 1rem;
+	}
+	.fundedheader {
+	  color: white;
+	  padding: 1rem;
+	}
+	.fundedcard {
+	  background-color: #161b22;
+	  border: 1px solid black;
+	  color: white;
+	  padding: 1rem;
+	}
+	.avatarrow {
+	  display: flex;
+	  flex-direction: row;
+	  grid-gap: 1rem;
+	}
+	.avatar {
+	  border-radius: 100%;
+	  width: 15vw;
+	  height: 15vw;
+	}
+	p, a {
+	  margin: 5px 0;
+	}
+	.links {
+	  display: flex;
+	  flex-direction: row;
+	  grid-gap: 10px;
+	}
+	a {
+	  color: white;
+	}
+	h4 {
+	  font-size: 1rem;
+	}
+	@media (min-width: 640px) { 
+	  .avatar {
+		width: 3vw;
+		height: 3vw;
+	  }
+	}
+  </style>
